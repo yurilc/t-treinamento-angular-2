7fd71195984f624947b8273d4214a8d9a7dd8f6b
Yuri Cavalcante <yurilimacavalcante@gmail.com>
2017-09-13 17:52:58 -0300

    finalizado componentes do ingresso

--- ingressos/src/app/ingressos/ingresso-form/ingresso-form.component.ts
+++ ingressos/src/app/ingressos/ingresso-form/ingresso-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnChanges, OnInit, Output, Input, EventEmitter } from '@angular/core';
 
 import { Ingresso } from '../ingresso.model';
 
@@ -7,21 +7,33 @@ import { Ingresso } from '../ingresso.model';
   templateUrl: './ingresso-form.component.html',
   styleUrls: ['./ingresso-form.component.css']
 })
-export class IngressoFormComponent implements OnInit {
+export class IngressoFormComponent implements OnChanges, OnInit {
 
   @Output('onSave') ingressoSalvo = new EventEmitter<Ingresso>();
+  @Output('onUpdate') ingressoAtualizado = new EventEmitter<Ingresso>();
   @Output('onCancel') cancelar = new EventEmitter();
 
+  @Input()
   ingresso: Ingresso = new Ingresso();
 
+  @Input() index: number;
+
   constructor() { }
+
+  ngOnChanges() {
+    console.log('OnChanges');
+  }
 
   ngOnInit() {
   }
 
   onSave() {
     console.log('IngressoFormComponent.onSave()', this.ingresso);
-    this.ingressoSalvo.emit(this.ingresso);
+    if(this.index > -1) {
+      this.ingressoAtualizado.emit(this.ingresso);
+    } else {
+      this.ingressoSalvo.emit(this.ingresso);
+    }
   }
 
   onCancel() {
--- ingressos/src/app/ingressos/ingresso-list.component.ts
+++ ingressos/src/app/ingressos/ingresso-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnChanges, OnInit, DoCheck, AfterContentInit,
+         AfterContentChecked, OnDestroy } from '@angular/core';
 
 import { Ingresso } from './ingresso.model';
 
@@ -6,9 +7,11 @@ import { Ingresso } from './ingresso.model';
     selector: 'app-ingresso-list',
     templateUrl: './ingresso-list.component.html'
 })
-export class IngressoListComponent {
+export class IngressoListComponent implements OnChanges, OnInit,
+    DoCheck, AfterContentInit, AfterContentChecked, OnDestroy {
 
-    modo = 'listagem';
+    ingressoSelecionado: Ingresso;
+    indexSelecionado = -1;
 
     ingressos: Ingresso[] = [
         new Ingresso('It', 23, 3, new Date(2017, 8, 15), '21:00'),
@@ -16,16 +19,55 @@ export class IngressoListComponent {
         new Ingresso('Transformers', 1.99, 1, new Date(2017, 8, 14), '18:35')
     ];
 
+    ngOnChanges() {
+        console.log('OnChanges');
+    }
+
+    ngOnInit() {
+        console.log('OnInit');
+    }
+
+    ngDoCheck() {
+        console.log('DoCheck');
+    }
+
+    ngAfterContentInit() {
+        console.log('AfterContentInit');
+    }
+
+    ngAfterContentChecked() {
+        console.log('AfterContentChecked');
+    }
+
+    ngOnDestroy() {
+        console.log('OnDestroy');
+    }
+
     onNew() {
-        this.modo = 'cadastro';
+        this.ingressoSelecionado = new Ingresso();
+        this.indexSelecionado = -1;
     }
 
     onSave(ingresso: Ingresso) {
         this.ingressos.push(ingresso);
-        this.modo = 'listagem';
+        this.ingressoSelecionado = undefined;
+    }
+
+    onUpdate(ingresso: Ingresso) {
+        this.ingressos.splice(this.indexSelecionado, 1, ingresso);
+        this.ingressoSelecionado = undefined;
     }
 
     onCancel() {
-        this.modo = 'listagem';
+        this.ingressoSelecionado = undefined;
+    }
+
+    onSelect(index: number) {
+        this.ingressoSelecionado = { ...this.ingressos[index] };
+        this.indexSelecionado = index;
+    }
+
+    onDelete(index: number) {
+        this.ingressos.splice(index, 1);
     }
 }
